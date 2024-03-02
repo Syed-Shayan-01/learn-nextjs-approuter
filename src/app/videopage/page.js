2765399fa9de8f86f8cbbd1be58ed56b85332830
Syed Shayan <shayansyed712@gmail.com>
2024-03-02 23:54:15 +0500

    change mistake class to className in videopage

--- src/app/videopage/page.js
+++ src/app/videopage/page.js
@@ -5,33 +5,33 @@ const page = () => {
             <div>
                 <Navbar />
             </div>
-            <div className="p-10 text-center text-[2.5rem] font-bold">
+            <div classNameName="p-10 text-center text-[2.5rem] font-bold">
                 Video Gallery
             </div>
-            <div class="flex flex-wrap items-center justify-evenly md:grid-cols-3 gap-4 p-12">
+            <div className="flex flex-wrap items-center justify-evenly md:grid-cols-3 gap-4 p-12">
                 <div>
-                    <iframe class="h-auto max-w-full rounded-lg" src="https://www.youtube.com/embed/bS1MLTPWVeA?si=ij2iAa9IAzH7uSkf" alt="" />
+                    <iframe className="h-auto max-w-full rounded-lg" src="https://www.youtube.com/embed/bS1MLTPWVeA?si=ij2iAa9IAzH7uSkf" alt="" />
                 </div>
                 <div>
-                    <iframe class="h-auto max-w-full rounded-lg" src="https://www.youtube.com/embed/bS1MLTPWVeA?si=ij2iAa9IAzH7uSkf" alt="" />
+                    <iframe className="h-auto max-w-full rounded-lg" src="https://www.youtube.com/embed/bS1MLTPWVeA?si=ij2iAa9IAzH7uSkf" alt="" />
                 </div>
                 <div>
-                    <iframe class="h-auto max-w-full rounded-lg" src="https://www.youtube.com/embed/bS1MLTPWVeA?si=ij2iAa9IAzH7uSkf" alt="" />
+                    <iframe className="h-auto max-w-full rounded-lg" src="https://www.youtube.com/embed/bS1MLTPWVeA?si=ij2iAa9IAzH7uSkf" alt="" />
                 </div>
                 <div>
-                    <iframe class="h-auto max-w-full rounded-lg" src="https://www.youtube.com/embed/bS1MLTPWVeA?si=ij2iAa9IAzH7uSkf" alt="" />
+                    <iframe className="h-auto max-w-full rounded-lg" src="https://www.youtube.com/embed/bS1MLTPWVeA?si=ij2iAa9IAzH7uSkf" alt="" />
                 </div>
                 <div>
-                    <iframe class="h-auto max-w-full rounded-lg" src="https://www.youtube.com/embed/bS1MLTPWVeA?si=ij2iAa9IAzH7uSkf" alt="" />
+                    <iframe className="h-auto max-w-full rounded-lg" src="https://www.youtube.com/embed/bS1MLTPWVeA?si=ij2iAa9IAzH7uSkf" alt="" />
                 </div>
                 <div>
-                    <iframe class="h-auto max-w-full rounded-lg" src="https://www.youtube.com/embed/bS1MLTPWVeA?si=ij2iAa9IAzH7uSkf" alt="" />
+                    <iframe className="h-auto max-w-full rounded-lg" src="https://www.youtube.com/embed/bS1MLTPWVeA?si=ij2iAa9IAzH7uSkf" alt="" />
                 </div>
                 <div>
-                    <iframe class="h-auto max-w-full rounded-lg" src="https://www.youtube.com/embed/bS1MLTPWVeA?si=ij2iAa9IAzH7uSkf" alt="" />
+                    <iframe className="h-auto max-w-full rounded-lg" src="https://www.youtube.com/embed/bS1MLTPWVeA?si=ij2iAa9IAzH7uSkf" alt="" />
                 </div>
                 <div>
-                    <iframe class="h-auto max-w-full rounded-lg" src="https://www.youtube.com/embed/bS1MLTPWVeA?si=ij2iAa9IAzH7uSkf" alt="" />
+                    <iframe className="h-auto max-w-full rounded-lg" src="https://www.youtube.com/embed/bS1MLTPWVeA?si=ij2iAa9IAzH7uSkf" alt="" />
                 </div>
 
             </div>
