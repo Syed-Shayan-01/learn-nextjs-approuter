e04558008adc9580b2a0c6eb2627d546b58e93a7
Syed Shayan <shayansyed712@gmail.com>
2024-04-07 22:23:44 +0500

    "Remove rmdir and access from fs/promises and update related code in image upload route"
    This commit removes the use of 'rmdir' and 'access' from 'fs/promises' and updates the related code in the image upload route. It also includes some commented out code, indicating potential changes that were considered but not implemented.

--- src/app/api/admin/imageUpload/route.js
+++ src/app/api/admin/imageUpload/route.js
@@ -1,4 +1,4 @@
-import { access, rmdir, unlink, writeFile } from 'fs/promises';
+import { access, unlink } from 'fs/promises';
 import { NextResponse } from 'next/server';
 import { v2 as cloudinary } from 'cloudinary';
 import Image from '@/models/imageSchema';
@@ -13,32 +13,13 @@ cloudinary.config({
 export const POST = async (request) => {
     await connectDB();
     try {
-        const file = await request.formData();
-        const data = await file.get('image');
-
-        if (!data) {
+        const data = await request.formData();
+        const file = await data.get('image');
+        if (!file) {
             return NextResponse.json("No Image Selected", { status: 400 });
         }
 
-        const byteData = await data.arrayBuffer();
-        const base64Data = Buffer.from(byteData);
-        const path = `./public/uploads/${data.name}`;
-
-        await writeFile(path, base64Data);
-
-        // Check if file exists in the specified path
-        const fileExists = await exists(path);
-
-        if (!fileExists) {
-            return NextResponse.json({
-                message: "Failed to save image to local folder",
-                success: false,
-                status: 500
-            });
-        }
-
-        // If image is successfully saved locally, upload it to Cloudinary
-        cloudinary.uploader.upload(path, async function (error, result) {
+        await cloudinary.uploader.upload(file.name, async function (error, result) {
             if (error) {
                 return NextResponse.json({
                     message: "Failed to upload image to Cloudinary",
@@ -54,9 +35,7 @@ export const POST = async (request) => {
             if (!imageSave) {
                 return NextResponse.json({ message: "Image url not save in Mongodb", status: 400 })
             }
-            // After successful upload to Cloudinary, delete the image from local directory
-            await unlink(path);
-            console.log("Image deleted from local directory.");
+
         });
 
         return NextResponse.json({
@@ -66,15 +45,5 @@ export const POST = async (request) => {
         });
     } catch (error) {
         return NextResponse.json({ message: error, status: 500 });
-    }
-};
-
-// Function to check if a file exists
-const exists = async (path) => {
-    try {
-        await access(path);
-        return true;
-    } catch (error) {
-        return false;
     }
 };
