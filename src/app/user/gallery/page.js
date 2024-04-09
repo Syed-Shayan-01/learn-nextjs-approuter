b924089024c79223829c5390500a9a76170062d2
Syed Shayan <shayansyed712@gmail.com>
2024-04-09 12:01:23 +0500

    "Added next/link for images in user gallery to open in new tab"
    This commit adds the Next.js Link component to each image in the user gallery, allowing them to open in a new tab when clicked. The Link component wraps the existing Image component and includes the image URL, target, and rel attributes.

--- src/app/user/gallery/page.js
+++ src/app/user/gallery/page.js
@@ -4,6 +4,7 @@ import PageName from "@/components/(user)/pageName/PageName";
 import axios from "axios";
 import Image from 'next/image';
 import { useEffect, useState } from "react";
+import Link from 'next/link'
 const Gallery = () => {
     const [images, setImages] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -37,14 +38,14 @@ const Gallery = () => {
             </div>
             <PageName pageNameText={'Image Gallery'} />
             <div className="grid grid-cols-2  md:grid-cols-3 gap-4 p-12">
-
                 {images && images.map((image) => (
-                    <div key={image._id}>
-                        <Image className="h-auto max-w-full rounded-lg" width={400} height={200} src={image.imageUrl} alt="" ></Image>
+                    <div key={image._id} className="hover:scale-110 duration-300">
+                        <Link href={image.imageUrl} target="_blank" rel="noopener noreferrer">
+                            <Image className="h-auto max-w-full rounded-lg" width={400} height={200} src={image.imageUrl} alt="" />
+                        </Link>
                     </div>)
                 )}
             </div>
-
         </>
     )
 }
